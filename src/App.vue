fce12f02f6dd8afb31a2c9bba48ad52ae8c42711
rhenaldkarrel <renaldkarel@gmail.com>
2025-08-20 12:51:42 +0700

    feat: initial form input task

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const newTask = ref('');
+
+function handleSubmit(event: Event) {
+	event.preventDefault();
+	if (newTask.value.trim()) {
+		console.log('New Task:', newTask.value);
+		newTask.value = '';
+	} else {
+		alert('Task cannot be empty');
+	}
+}
+</script>
 
 <template>
-	<h1>Todo App</h1>
+	<main>
+		<h1>Todo App</h1>
+
+		<form @submit="handleSubmit">
+			<input type="text" v-model="newTask" placeholder="Add a new todo" />
+			<div class="btn-add-todo-container">
+				<button>Add</button>
+			</div>
+		</form>
+		<p>{{ newTask }}</p>
+	</main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+	max-width: 50rem;
+	margin: 0 auto;
+}
+
+.btn-add-todo-container {
+	display: flex;
+	justify-content: end;
+}
+
+form > *:where(:not(:last-child)) {
+	margin-bottom: 1rem;
+}
+</style>
