66682309e40b162308c1bc64170b7f10c186ed8b
rhenaldkarrel <renaldkarel@gmail.com>
2025-08-21 14:42:55 +0700

    feat: add `TaskCard` component

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,46 @@
+<template>
+	<label class="task-card">
+		<input type="checkbox" v-model="task.completed" class="task-checkbox" />
+		{{ task.name }}
+	</label>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+import type { Task } from '../types';
+
+defineProps<{
+	task: Task;
+}>();
+</script>
+
+<style scoped>
+.task-checkbox {
+	width: auto;
+}
+
+.task-card {
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	background-color: #323232;
+	border-radius: 0.5rem;
+	padding: 1rem;
+	color: #fff;
+	border: 1px solid red;
+	cursor: pointer;
+	transition: all 0.3s ease;
+}
+
+.task-card:hover {
+	border-color: green;
+}
+
+@media (prefers-color-scheme: light) {
+	.task-card {
+		background-color: #f9f9f9;
+		color: #000;
+	}
+}
+</style>
